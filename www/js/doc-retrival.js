986dcad51991d62b6d7d14c5244d1656841f0f65
Yaseen Shaikh <yaseentheepic@gmail.com>
2018-04-06 06:21:01 -0700

    Initial work on doc retrival
    Signed-off-by: Yaseen Shaikh <yaseentheepic@gmail.com>

--- www/js/doc-retrival.js
+++ www/js/doc-retrival.js
@@ -8,15 +8,41 @@ var config = {
     storageBucket: "techno-titans-app.appspot.com",
     messagingSenderId: "229979215973"
 };
+
+const Section = Object.freeze({
+    PROGRAMMING: "programming",
+    MECHANICAL: "mechanical",
+    BUSINESS: "business",
+    SAFETY: "safety",
+    ELECTRICAL: "electrical",
+    COMPETITION: "competition",
+    GENERAL: "general"
+});
+
 firebase.initializeApp(config);
+var storage = firebase.storage();
 
 var allDocs = [];
 
+// iterate through the folders and get all the urls
+// we already know the folder structure, but we don't know article names,
+// so we can just iterate over it
+// SHOULD BE CALLED AS SOME SORT OF PREFETCH OR `window.onload` or something
+
+var getDocUrls = function () {
+    var tempDocArray = [];
+    return new Promise(resolve => {
+        Section.forEach((section) => {
+            // tempDocArray.push(storage.ref().child('documents').child(section) // TODO iterate over articles and
+        });
+        resolve(tempDocArray);
+    })
+};
+
 var parseDocument = function () {
 
-}
+};
 
 var getAllDocs = function() {
 
-}
-
+};
